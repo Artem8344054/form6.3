2b29f0a9566cb63b95930859c01c1dd242113452
Artem Morozov <bars834@gmail.com>
2023-03-10 00:17:45 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function () {
 		var form = jQuery(this).closest('form');
 
 		if (form.valid()) {
-			// form.css('opacity', '.5');
+			form.css('opacity', '.5');
 			var actUrl = form.attr('action');
 
 			jQuery.ajax({
@@ -16,10 +16,10 @@ jQuery(document).ready(function () {
 				dataType: 'html',
 				data: form.serialize(),
 				success: function (data) {
-					// form.html(data);
-					// form.css('opacity', '1');
-					form.find('.status').html('form submitted successfully!!!');
-					$('#exampleModal').modal('show') // для бутстрапа
+					form.html(data);
+					form.css('opacity', '1');
+					// form.find('.status').html('form submitted successfully!!!');
+					// $('#exampleModal').modal('show') // для бутстрапа
 				},
 				error: function () {
 					form.find('.status').html('Ошибка сервера');
